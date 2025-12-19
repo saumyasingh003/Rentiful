f9169eca54669d9aa77913828812d5cbb10b0996
saumyasingh003 <saumyasingh98982@gmail.com>
2025-12-19 22:49:09 +0530

    updates

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,1 +1,1 @@
-module.exports = { apps : [{ name: 'rentiful', script: 'npm', args: 'run dev', env: { NODE_ENV: 'development'} }], };
+module.exports = { apps : [{ name: 'rentiful', script: 'npm', args: 'run dev', env: { NODE_ENV: 'development', NODE_EXTRA_CA_CERTS: "/etc/ssl/aws/rds-combined-ca-bundle.pem"} }], };
